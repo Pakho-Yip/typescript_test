b68644fe8ca0382659e63af4c0c65270911c5299
Pakho Yip <943389417@qq.com>
2024-03-11 22:52:53 +0800

    07_TS编译选项（3）

--- chapter01/part2/dist/app.js
+++ chapter01/part2/dist/app.js
@@ -1,31 +1,4 @@
-System.register("m", [], function (exports_1, context_1) {
-    "use strict";
-    var hi, b, c;
-    var __moduleName = context_1 && context_1.id;
-    return {
-        setters: [],
-        execute: function () {
-            exports_1("hi", hi = '你好');
-            b = 20;
-            c = 'hello';
-            console.log(b, c);
-        }
-    };
-});
-System.register("app", ["m"], function (exports_2, context_2) {
-    "use strict";
-    var m_js_1, a;
-    var __moduleName = context_2 && context_2.id;
-    return {
-        setters: [
-            function (m_js_1_1) {
-                m_js_1 = m_js_1_1;
-            }
-        ],
-        execute: function () {
-            a = 10;
-            console.log(m_js_1.hi);
-            console.log(a);
-        }
-    };
-});
+import { hi } from './m.js';
+let a = 10;
+console.log(hi);
+console.log(a);
--- chapter01/part2/src/app.ts
+++ chapter01/part2/src/app.ts
@@ -1,4 +1,6 @@
 import {hi} from './m.js'
 let a = 10;
+// 这是一个注释
 console.log(hi);
 console.log(a);
+// a = 'hello';
