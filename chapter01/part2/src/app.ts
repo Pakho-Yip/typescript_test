70b559fdaa06b617e12f3b43887952564fb57274
Pakho Yip <943389417@qq.com>
2024-03-11 23:48:28 +0800

    08_TS编译选项（4）

--- chapter01/part2/src/app.ts
+++ chapter01/part2/src/app.ts
@@ -4,3 +4,23 @@ let a = 10;
 console.log(hi);
 console.log(a);
 // a = 'hello';
+
+function fn(a:number,b:number){
+    return a + b
+}
+
+function fn2(this:Window){
+    alert(this)
+}
+
+let box1 = document.getElementById('box1');
+
+// if(box1!=null){
+//     box1.addEventListener('click',function(){
+//         alert('hello');
+//     });
+// }
+
+box1?.addEventListener('click',function(){
+    alert('hello');
+});
