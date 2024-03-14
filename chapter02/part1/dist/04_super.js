8ee90f6e30a4d613b824c4e2d4b384b9024e94b3
qingshuang.ye <yeqingshuan@tcsc.com>
2024-03-14 16:46:57 +0800

    test

--- chapter02/part1/dist/04_super.js
+++ chapter02/part1/dist/04_super.js
@@ -11,7 +11,7 @@
     class Dog extends Animal {
         constructor(name, age) {
             // 如果在子类中写了构造函数，在子类构造函数中必须对父类的构造函数进行调用
-            super(name);
+            super(name); // 调用父类的构造函数
             this.age = age;
         }
         sayHello() {
